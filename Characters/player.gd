363f6472b627d268844f112c7de852b10950f2e8
Nigel Massey <nigelmassey90@gmail.com>
2023-10-28 13:09:20 +0100

    jumping and attacking

--- Characters/player.gd
+++ Characters/player.gd
@@ -10,15 +10,22 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animationLocked : bool = false
 var direction : float = 0
+var is_jumping : bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if velocity.y > 0:
+			animatedSprite.play("fall")			
+	elif is_jumping:
+		animatedSprite.play("ground")
+		is_jumping = false
+		
+	jump()
+		
+	if Input.is_action_just_pressed("attack"):
+		attack()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -28,16 +35,31 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	update_animation()
 	move_and_slide()
+	update_animation()
 
 func update_animation():
 	if not animationLocked:
 		if velocity.x == 0:
 			animatedSprite.play("idle")
-		elif velocity.x != 0:
+		elif velocity.x != 0 && not is_on_wall():
 			animatedSprite.play("run")
 			if direction == 1:
 				animatedSprite.flip_h = false
 			elif direction == -1:
 				animatedSprite.flip_h = true
+
+func attack():
+	animationLocked = true
+	animatedSprite.play("attack")
+	
+func jump():	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animationLocked = true
+		is_jumping = true
+		animatedSprite.play("jump")
+		velocity.y = JUMP_VELOCITY
+	
+func _on_animated_sprite_2d_animation_finished():
+	if not is_jumping:
+		animationLocked = false
